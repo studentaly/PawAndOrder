3023154d7e9fb0504d590e20b64d56335487cb52
Taylor Reed <81502909+taynicreed@users.noreply.github.com>
2023-02-19 10:13:54 -0800

    Update PawAndOrder.sql ~ step 3 draft code

--- PawAndOrder.sql
+++ PawAndOrder.sql
@@ -1,7 +1,18 @@
--- Create tables
+-- -----------------------------------------------------
+-- Group 150 ~ Alyssa Feutz & Taylor Reed
+-- Data Definition Queries
+-- -----------------------------------------------------
 
-CREATE TABLE Customers (
-    customerID int NOT NULL AUTO_INCREMENT,
+SET FOREIGN_KEY_CHECKS = 0;
+SET AUTOCOMMIT = 0;
+
+-- -----------------------------------------------------
+-- Creating Tables
+-- -----------------------------------------------------
+
+-- Create Customers table
+CREATE OR REPLACE TABLE Customers (
+    customerID int(11) NOT NULL AUTO_INCREMENT,
     customerName varchar(50) NOT NULL,
     altName varchar(50) NULL,
     phone varchar(20) NOT NULL,
@@ -9,34 +20,38 @@ CREATE TABLE Customers (
     PRIMARY KEY (customerID)
 );
 
-CREATE TABLE Dogs (
-    dogID int NOT NULL AUTO_INCREMENT,
-    customerID int NOT NULL,
+-- Create Dogs table
+CREATE OR REPLACE TABLE Dogs (
+    dogID int(11) NOT NULL AUTO_INCREMENT,
+    customerID int(11) NOT NULL,
     dogName varchar(50) NOT NULL,
     dogBirthday date NOT NULL,
-    active boolean,
+    active tinyint(1),
     PRIMARY KEY (dogID),
     FOREIGN KEY (customerID) REFERENCES Customers(customerID) ON DELETE CASCADE
 );
 
-CREATE TABLE Employees (
-    employeeID int NOT NULL AUTO_INCREMENT,
+-- Create Employees table
+CREATE OR REPLACE TABLE Employees (
+    employeeID int(11) NOT NULL AUTO_INCREMENT,
     employeeName varchar(50) NOT NULL,
     employeeType varchar(50) NOT NULL,
     PRIMARY KEY (employeeID)
 );
 
-CREATE TABLE Commands (
-    commandID int NOT NULL AUTO_INCREMENT,
+-- Create Commands table
+CREATE OR REPLACE TABLE Commands (
+    commandID int(11) NOT NULL AUTO_INCREMENT,
     commandName varchar(50) NOT NULL,
     PRIMARY KEY (commandID)
 );
 
-CREATE TABLE TrainingSessions (
-    sessionID int NOT NULL AUTO_INCREMENT,
-    customerID int NOT NULL,
-    dogID int NOT NULL,
-    employeeID int NOT NULL,
+-- Create TrainingSessions table
+CREATE OR REPLACE TABLE TrainingSessions (
+    sessionID int(11) NOT NULL AUTO_INCREMENT,
+    customerID int(11) NOT NULL,
+    dogID int(11) NOT NULL,
+    employeeID int(11) NOT NULL,
     sessionDate date NOT NULL,
     notes varchar(300) NOT NULL,
     PRIMARY KEY (sessionID),
@@ -46,35 +61,43 @@ CREATE TABLE TrainingSessions (
     ON DELETE CASCADE
 );
 
-CREATE TABLE CommandsLearned (
-  commandsLearnedByDog varchar(50),
-  commandID int NOT NULL,
-  dogID int NOT NULL,
-  PRIMARY KEY (commandsLearnedByDog),
+-- Create CommandsLearned table
+CREATE OR REPLACE TABLE CommandsLearned (
+  CommandsLearnedByDog int(11) NOT NULL AUTO_INCREMENT,
+  commandID int(11) NOT NULL,
+  dogID int(11) NOT NULL,
+  PRIMARY KEY (CommandsLearnedByDog),
   FOREIGN KEY (commandID) REFERENCES Commands(commandID),
   FOREIGN KEY (dogID) REFERENCES Dogs(dogID)
   ON DELETE CASCADE
 
 );
 
+-- -----------------------------------------------------
 -- Insert Example Data
+-- -----------------------------------------------------
+
+-- Insert Customers data
 INSERT INTO Customers (customerName, altName, phone, email)
-    VALUES ('Martina Galeano','Alyssa Feutz','555-555-5555', 'martina@gmail.com'),
+    VALUES ('Martina Hansen','Alyssa Hansen','555-555-5555', 'martina@gmail.com'),
     ('Gail Smith', 'Brenda Smith', '555-555-5556', 'gail@gmail.com'),
     ('Alex Baker', NULL, '555-555-5557', NULL);
 
+-- Insert Dogs data
 INSERT INTO Dogs (customerID, dogName, dogBirthday, active)
-VALUES (1, 'Surco', '2022-11-10', 1),
+VALUES (1, 'Spot', '2022-10-20', 1),
 (2, 'Josh', '2016-02-01', 1),
 (2, 'Potato', '2019-05-07', 1),
 (3, 'George', '2013-01-21', 0),
 (3, 'Echo', '2015-08-13', 0);
 
+-- Insert Employees data
 INSERT INTO Employees (employeeName, employeeType)
 VALUES ('Jaime Gomez', 'Trainer'),
 ("Rachel Li", "Office Manager"),
 ("Deborah Pasic", "Trainer");
 
+-- Insert Commands data
 INSERT INTO Commands (commandName)
 VALUES ('Sit'),
 ('Shake'),
@@ -82,6 +105,7 @@ VALUES ('Sit'),
 ('Heal'),
 ('Come');
 
+-- Insert TrainingSessions data
 INSERT INTO TrainingSessions (customerID, dogID, employeeID, sessionDate, notes)
 VALUES (1, 1, 1, "2023-02-04", "Good Boy"),
 (2, 2, 1, "2023-02-04", "Good Boy"),
@@ -89,8 +113,13 @@ VALUES (1, 1, 1, "2023-02-04", "Good Boy"),
 (1, 1, 3, "2023-02-05", "Ate all the treats, order more"),
 (2, 2, 3, "2023-02-05", "Need to revisit down command");
 
-INSERT INTO CommandsLearned (commandsLearnedByDog, commandID, dogID)
-VALUES ("SurcoSit", 1, 1),
-("SurcoDown", 3, 1),
-("JoshHeal", 4, 2),
-("JoshCome", 5, 2);
+-- Insert CommandsLearned data
+INSERT INTO CommandsLearned (commandID, dogID)
+VALUES (1, 1),
+(3, 1),
+(4, 2),
+(5, 2);
+
+
+SET FOREIGN_KEY_CHECKS = 1;
+COMMIT;

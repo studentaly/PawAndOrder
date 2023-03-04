276b81823bd6ada0ef9cdb59bec883ed773cbd41
alyssafeutz <feutza@oregonstate.edu>
2023-03-04 14:46:13 -0800

    update for new entities

--- app.py
+++ app.py
@@ -143,8 +143,132 @@ def editCustomer(customerID):
 
     return redirect("/customers")
 
+@app.route('/employees', methods=["POST", "GET"])
+def employees():
+    if request.method == "GET":
+        query = "SELECT* FROM Employees"
+        cur = mysql.connection.cursor()
+        cur.execute(query)
+        data= cur.fetchall()
+
+        return render_template("employees.j2", data=data)
+
+    if request.method == "POST":
+        # When user adds employee
+        if request.form.get("Add Employee"):
+            # user form inputs
+            employeeName = request.form["employeeName"]
+            employeeType = request.form["employeeType"]
+
+            # All inputs are required
+
+            query = "INSERT INTO Employees (employeeName, employeeType) VALUES (%s, %s)"
+            cur = mysql.connection.cursor()
+            cur.execute(query, (employeeName, employeeType))
+            mysql.connection.commit()
+
+            # redirect back to employees
+            return redirect("/employees")
+
+@app.route('/commands', methods=["POST", "GET"])
+def commands():
+    if request.method == "GET":
+        query = "SELECT* FROM Commands"
+        cur = mysql.connection.cursor()
+        cur.execute(query)
+        data= cur.fetchall()
+
+        return render_template("commands.j2", data=data)
+
+    if request.method == "POST":
+        # When user adds employee
+        if request.form.get("Add Command"):
+            # user form inputs
+            commandName = request.form["commandName"]
+
+            # All inputs are required
+
+            query = "INSERT INTO Commands (commandName) VALUES (%s)"
+            cur = mysql.connection.cursor()
+            cur.execute(query, ([commandName]))
+            mysql.connection.commit()
+
+            # redirect back to commands
+            return redirect("/commands")
+
+@app.route('/commandsLearned', methods=["POST", "GET"])
+def commandsLearned():
+    if request.method == "GET":
+        query = "SELECT* FROM CommandsLearned"
+        cur = mysql.connection.cursor()
+        cur.execute(query)
+        data= cur.fetchall()
+
+        query2 = "SELECT commandID, commandName FROM Commands"
+        cur = mysql.connection.cursor()
+        cur.execute(query2)
+        commandData = cur.fetchall()
+
+        query3 = "SELECT dogID, dogName FROM Dogs"
+        cur = mysql.connection.cursor()
+        cur.execute(query3)
+        dogData = cur.fetchall()
+
+        return render_template("commandsLearned.j2", data=data, commandData=commandData, dogData=dogData)
+
+    if request.method == "POST":
+        # When user adds commandbydog
+        if request.form.get("Add CommandByDog"):
+            # user form inputs
+            commandID = request.form["commandID"]
+            dogID = request.form["dogID"]
+
+            # All inputs are required
+
+            query = "INSERT INTO CommandsLearned (commandID, dogID) VALUES (%s, %s)"
+            cur = mysql.connection.cursor()
+            cur.execute(query, (commandID, dogID))
+            mysql.connection.commit()
+
+            # redirect back to commandslearned
+            return redirect("/commandsLearned")
+
+@app.route('/dogs', methods=["POST", "GET"])
+def dogs():
+    if request.method == "GET":
+        query = "SELECT* FROM Dogs"
+        cur = mysql.connection.cursor()
+        cur.execute(query)
+        data= cur.fetchall()
+
+        query2 = "SELECT customerID, customerName FROM Customers"
+        cur = mysql.connection.cursor()
+        cur.execute(query2)
+        customerData = cur.fetchall()
+
+        return render_template("dogs.j2", data=data, customerData=customerData)
+
+    if request.method == "POST":
+        # When user adds dog
+        if request.form.get("Add Dog"):
+            # user form inputs
+            customerID = request.form["customerID"]
+            dogName = request.form["dogName"]
+            dogBirthday = request.form["dogBirthday"]
+            active = request.form["active"]
+
+            # All inputs are required
+
+            query = "INSERT INTO Dogs (customerID, dogName, dogBirthday, active) VALUES (%s, %s, %s, %s)"
+            cur = mysql.connection.cursor()
+            cur.execute(query, (customerID, dogName, dogBirthday, active))
+            mysql.connection.commit()
+
+            # redirect back to dogs
+            return redirect("/dogs")
+
 # Listener
 
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 9114))
-    app.run(host="flip1.engr.oregonstate.edu", port=port, debug=False)
+    app.run(host="flip1.engr.oregonstate.edu", port=port, debug=True)
